c168deeaa88d09957fbe8c526793db54cf366bd6
liue853224@gmail.com <liue853224@gmail.com>
2024-10-23 19:30:51 +0800

    sequelize init and add user model

--- servers/app.js
+++ servers/app.js
@@ -1,7 +1,11 @@
+// 加入環境變數
 require('dotenv').config()
+// 載入express、設置port
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+// 設置node內建path模組
+const path = require('path')
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
