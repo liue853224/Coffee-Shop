ac8e6efca452bc0423520295f02dd10eeea7cd96
liue853224@gmail.com <liue853224@gmail.com>
2024-11-04 21:05:06 +0800

    Modify add-favorite API for confirm favorite existing

--- servers/controllers/favorite-controller.js
+++ servers/controllers/favorite-controller.js
@@ -2,12 +2,30 @@ const { Favorite } = require("../models");
 
 const favoriteController = {
   addFavorite: (req, res, next) => {
+    const userId = req.user.id;
     const { productId } = req.body;
-    Favorite.create({ userId: req.user.id, productId })
-      .then((favorited) => {
-        res.status(201).json({
-          status: "success",
-          data: favorited,
+    if (!productId) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "productId不存在" });
+    }
+    Favorite.findOne({
+      where: {
+        userId,
+        productId,
+      },
+    })
+      .then((existingFavorite) => {
+        if (existingFavorite) {
+          return res
+            .status(409)
+            .json({ status: "errror", message: "已經加入最愛了" });
+        }
+        return Favorite.create({ userId, productId }).then((favorited) => {
+          res.status(201).json({
+            status: "success",
+            data: favorited,
+          });
         });
       })
       .catch((err) => {
